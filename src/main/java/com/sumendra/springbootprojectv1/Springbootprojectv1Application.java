5af4877ac9f19f48e8f87fc27db3227361afad39
Sumendra Chowdary <sumendrachowdary@gmail.com>
2021-12-12 18:19:13 +0530

    First commit of initialized project

---
+++ src/main/java/com/sumendra/springbootprojectv1/Springbootprojectv1Application.java
@@ -0,0 +1,13 @@
+package com.sumendra.springbootprojectv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootprojectv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootprojectv1Application.class, args);
+	}
+
+}
